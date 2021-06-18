4d5af1b9cb67bf98e7de68a84381a150ce2610bd
Drew Chandler <drew.chandler@dockyard.com>
2021-06-18 15:30:27 -0400

    Change exported interface

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const babel = require("@babel/core");
 const babelPlugin = require("./babel-plugin");
 
-module.exports = {
-  name: "svelte-inline-compile",
-  tranform(code) {
-    return babel.transform(code, { plugins: [babelPlugin] }).code;
-  },
+module.exports = function tranform(code) {
+  return babel.transform(code, { plugins: [babelPlugin] });
 };
